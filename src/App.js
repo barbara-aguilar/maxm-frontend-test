16c368957c140efcecffd0e4ba98a641f87d83c4
barbara-aguilar <bahlutz.design@gmail.com>
2019-01-17 12:59:11 -0200

    add main content and post component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
+import Post from './components/post';
+import './App.css'
+import Main from './components/main';
 
 
 class App extends Component {
@@ -8,7 +11,11 @@ class App extends Component {
     return (
       <React.Fragment>
         <Navbar/>
-
+      
+          <Main>
+            <Post/>
+          </Main>
+          
         <Footer/>
       </React.Fragment>
       
---
+++ src/components/post/post.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Post extends React.Component {
+  render() {
+    return  <React.Fragment>
+            <h1 className='post__title'>Hello World</h1>
+            <h3 className='post__subtitle'>diuhdiauiufhuhiusdiufhsiaishdfiuh</h3>
+            <p>gtagaugaygaugaugauaguaguyaguya</p>
+            </React.Fragment>
+    
+    }
+}
+
+export default Post
